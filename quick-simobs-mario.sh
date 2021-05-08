69696d7b1ff9d36eb83aad564c8009758935f8cc
Yuping Huang <yupinghyper@gmail.com>
2021-05-08 16:30:43 -0700

    Scripts update.

---
+++ quick-simobs-mario.sh
@@ -0,0 +1,33 @@
+dir='/fastpool/data/AJ-15int-4000chan'
+conf='20210226-configs/20210226M.cfg'
+
+python simobs.py $dir/00.ms -450s -390s $conf &
+# Sleep seems to be needed so that log files are separate
+sleep 2
+python simobs.py $dir/01.ms -390s -330s $conf &
+sleep 2
+python simobs.py $dir/02.ms -330s -270s $conf &
+sleep 2
+python simobs.py $dir/03.ms -270s -210s $conf &
+sleep 2
+python simobs.py $dir/04.ms -210s -150s $conf &
+sleep 2
+python simobs.py $dir/05.ms -150s -90s $conf &
+sleep 2
+python simobs.py $dir/06.ms -90s -30s $conf &
+sleep 2
+python simobs.py $dir/07.ms -30s 30s $conf &
+sleep 2
+python simobs.py $dir/08.ms 30s 90s $conf &
+sleep 2
+python simobs.py $dir/09.ms 90s 150s $conf &
+sleep 2
+python simobs.py $dir/10.ms 150s 210s $conf &
+sleep 2
+python simobs.py $dir/11.ms 210s 270s $conf &
+sleep 2
+python simobs.py $dir/12.ms 270s 330s $conf &
+sleep 2
+python simobs.py $dir/13.ms 330s 390s $conf &
+sleep 2
+python simobs.py $dir/14.ms 390s 450s $conf &
--- simobs.py
+++ simobs.py
@@ -27,7 +27,7 @@ me = casatools.measures()
 sm.open(msname)
 pos_ovro_mma=me.observatory('ovro_mma')
 sm.setconfig(telescopename='ovro_mma', x=xx, y=yy, z=zz, dishdiameter=diam, mount='alt-az', antname=list(anames), padname=list(anames), coordsystem='local', referencelocation=pos_ovro_mma)
-sm.setspwindow(spwname='LBand', freq='1.35GHz', deltafreq='0.8125MHz', freqresolution='0.8125MHz', nchannels=1, stokes='RR RL LR LL')
+sm.setspwindow(spwname='LBand', freq='0.7GHz', deltafreq='0.8125MHz', freqresolution='0.325MHz', nchannels=4000, stokes='RR RL LR LL')
 sm.setfeed('perfect R L')
 # With rotated.cfg I got 6.11x6.02 with natural weighting.
 # sm.setfield(sourcename='source', sourcedirection=['J2000', '00h00m0.0', '+85.00.00.000'])
