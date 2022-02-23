68ad37eac74bbb59057779bced6aba2e8f55a115
Yuping Huang <yupinghyper@gmail.com>
2022-02-23 09:37:11 -0800

    Pointing error fix.

--- notebooks/pointing.py
+++ notebooks/pointing.py
@@ -5,7 +5,6 @@ import numpy as np
 def get_pointing_error(error_rms_deg, size, generator: Optional[Generator]=None):
     if not generator:
         generator = default_rng()
-    # should be fine since 1 arcmin = 3e-4 rad
-    sep_deg = generator.normal(loc=0.0, scale=error_rms_deg/np.sqrt(2), size=size)
-    pa_deg = generator.normal(loc=0.0, scale=error_rms_deg/np.sqrt(2), size=size)
+    sep_deg = generator.normal(loc=0.0, scale=error_rms_deg, size=size)
+    pa_deg = generator.uniform(low=0.0, high=360, size=size)
     return sep_deg, pa_deg
